45dd4922e087c66b785ca4cdee3b638e33c36362
AndBarDev <134181548+andresbaron18@users.noreply.github.com>
2026-06-13 10:10:05 -0500

    complete exercise 1.3: object restructuring and dot notation

--- part1/courseinfo/1.1_Course_Information_step_1/src/App.jsx
+++ part1/courseinfo/1.1_Course_Information_step_1/src/App.jsx
@@ -28,25 +28,34 @@ const Total = (props)=>{
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7 
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const course= 'Half Stack application development'
+
+  const part1 = {
+    part: 'Fundamentals of React',
+    exercises: 10
+  }
+    
+  const part2 = {
+    part: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    part: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-      part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2}
-      part3={part3} exercises3={exercises3}
+      part1={part1.part} exercises1={part1.exercises}
+      part2={part2.part} exercises2={part2.exercises}
+      part3={part3.part} exercises3={part3.exercises}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
 
 export default App
+
