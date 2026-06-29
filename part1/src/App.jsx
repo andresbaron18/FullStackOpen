af6279053be642a73fd9b2b614121c623708a17d
AndBarDev <134181548+andresbaron18@users.noreply.github.com>
2026-06-29 01:06:09 -0500

    Complete Full Stack Open exercise 1.7 Unicafe step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -15,18 +15,27 @@ const App = () =>{
      const badClick = ()=>{
     setBad(bad +1)
     } 
+    const all = good + neutral + bad
+     
+    const positive = all === 0 ? 0 :(good / all) * 100
+    const average =  all === 0 ? 0 :((good*1)+(neutral*0)+(bad*-1))/all
  return(
     <div>
          <button onClick={goodClick} >good </button>  
          <button onClick={neutralClick}>neutral</button>        
          <button onClick={badClick}>bad</button>
 
+         <h1>give feedback</h1>
+         <h2>statistics</h2>
+
          <p>Good {good}</p> 
          <p>Neutral {neutral}</p>
          <p>Bad {bad}</p>
+         <p>all {all} </p>
+         <p>positive {positive}% </p>
+         <p>average {average} </p>
 
     </div>
  )
 }
-
 export default App
