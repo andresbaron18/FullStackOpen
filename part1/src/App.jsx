edd2f29492c151bd551815dcbfc493582509dd92
AndBarDev <134181548+andresbaron18@users.noreply.github.com>
2026-06-30 00:29:10 -0500

    complete exercise 1.8: extract statistics into a separate component using props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,41 +1,65 @@
 import { useState } from "react"
 
-const App = () =>{
-    const [ good, setGood] = useState(0)
-    const [neutral, setNeutral]= useState(0)
-    const [bad, setBad]= useState(0)
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  const goodClick = () => {
+    setGood(good + 1)
+  }
 
-    const goodClick = ()=>{
-    setGood(good +1)
-    } 
-    const neutralClick = ()=>{
-    setNeutral(neutral +1)
-    } 
-     const badClick = ()=>{
-    setBad(bad +1)
-    } 
-    const all = good + neutral + bad
-     
-    const positive = all === 0 ? 0 :(good / all) * 100
-    const average =  all === 0 ? 0 :((good*1)+(neutral*0)+(bad*-1))/all
- return(
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+  const all = good + neutral + bad
+
+  const positive = all === 0
+    ? 0
+    : (good / all) * 100
+
+  const average = all === 0
+    ? 0
+    : ((good * 1) + (neutral * 0) + (bad * -1)) / all
+
+  return (
     <div>
-         <button onClick={goodClick} >good </button>  
-         <button onClick={neutralClick}>neutral</button>        
-         <button onClick={badClick}>bad</button>
+      <h1>give feedback</h1>
 
-         <h1>give feedback</h1>
-         <h2>statistics</h2>
+      <button onClick={goodClick}>good</button>
+      <button onClick={neutralClick}>neutral</button>
+      <button onClick={badClick}>bad</button>
 
-         <p>Good {good}</p> 
-         <p>Neutral {neutral}</p>
-         <p>Bad {bad}</p>
-         <p>all {all} </p>
-         <p>positive {positive}% </p>
-         <p>average {average} </p>
+      <h1>statistics</h1>
 
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </div>
- )
+  )
 }
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive}%</p>
+    </div>
+  )
+}
+
 export default App
