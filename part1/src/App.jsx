4e59231f9553ab6df389a8b692a1248c27e981a2
AndBarDev <134181548+andresbaron18@users.noreply.github.com>
2026-07-01 23:42:28 -0500

    Complete Full Stack Open exercise 1.12 anecdotes step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,89 +1,30 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const goodClick = () => {
-    setGood(good + 1)
+     const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-
-  const neutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const badClick = () => {
-    setBad(bad + 1)
-  }
- 
-  const all = good + neutral + bad
-
-  const positive = all === 0
-    ? 0
-    : (good / all) * 100
-
-  const average = all === 0
-    ? 0
-    : ((good * 1) + (neutral * 0) + (bad * -1)) / all
-
   return (
     <div>
-      <h1>give feedback</h1>
-
-      <button onClick={goodClick}>good</button>
-      <button onClick={neutralClick}>neutral</button>
-      <button onClick={badClick}>bad</button>
-
-      <h1>statistics</h1>
-
-    {
-       all === 0 
-        ? 
-        <p>No feedback given</p>
-        :<Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-        average={average}
-        positive={positive}
-      />
-    }   
+        <h1>Anecdote of the day</h1>
+       {anecdotes[selected]}
+       
+      <button onClick={nextAnecdote}>next anecdote</button>
     </div>
   )
-}
-
-const  Statistics = (props) => {
-  
-    return (
-       <table>
-
-      <tbody>
-      <StatisticsLine text="good" value={props.good} />
-      <StatisticsLine text="neutral" value={props.neutral} />
-      <StatisticsLine text="bad" value={props.bad} />
-      <StatisticsLine text="all" value={props.all} />
-      <StatisticsLine text="average" value={props.average} />
-      <StatisticsLine text="positive" value={props.positive + " %"} />
-      
-       </tbody>
-       </table>
-     
-      
-  )
-
-    }
-const StatisticsLine = (props) =>{
-  return(
-  
-      <tr>
-          <td>{props.text}</td>
-          <td>{props.value}</td>
-      </tr>
-   
-  )
-
 }
 
 export default App
