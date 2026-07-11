e09ba1a7f0fa6c3d3be96e8caf8891a3d92fadb6
AndBarDev <134181548+andresbaron18@users.noreply.github.com>
2026-07-11 17:32:54 -0500

    feat: complete Full Stack Open Part 2 exercises 2.1-2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,73 @@
 import { useState } from 'react'
 import './App.css'
+import { Fragment } from 'react';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-  const Course = ({ course }) => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const Courses = ({ courses }) => {
     return(
-      <>
-      <Header name={course.name}></Header>
-      <Content parts= {course.parts} ></Content>
-      <TotalExercises parts= {course.parts}></TotalExercises>
-      </>
+      courses.map(curso => (
+        <Fragment key={curso.id}>
+        
+        <Header name={curso.name} ></Header>
+        <Content parts= {curso.parts} ></Content>
+        <TotalExercises parts= {curso.parts}></TotalExercises>
+        </Fragment>
+        
+      ))
+     
     )
    }  
     const Header = ({name}) => {
       return (
-        <h1> {name} </h1>
+         <h1> {name} </h1>
     )
    }
+
+ 
    const Content = ({parts}) => {
     return(
       parts.map(part =>(
@@ -61,9 +92,7 @@ const App = () => {
         return(
          <p> total of {total} exercises</p>
            )
-      }
-    
-
-  return <Course course={course} />
+      } 
+  return <Courses courses={courses} />
 }
 export default App
